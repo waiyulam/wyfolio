4ce06084ca9a49648871d0f22a831f0c2f6bcb5c
waiyulam <waiyu0616@outlook.com>
2022-06-27 15:43:26 -0400

    Add new not found page

---
+++ src/components/error/Error.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Fade } from "react-awesome-reveal";
+import "./Error.css";
+import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Particle from "../Particle";
+
+function Error() {
+  return(
+      <Container fluid className = "error-main" >
+        <Particle />
+        <Container className="error-class">
+          <Fade bottom duration={2000} distance="40px">
+            <h1>Woops</h1>
+            <p>The requested page is unavailable at the moment!</p>
+            <Link
+              className="main-button"
+              to="/home"
+              style={{
+                color: "purple",
+                backgroundColor: "white",
+                border: `solid 1px ${"white"}`,
+                display: "inline-flex",
+              }}
+            >
+              Go Home
+            </Link>
+          </Fade>
+        </Container>
+      </Container>
+  );
+}
+
+export default Error
